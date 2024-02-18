7697c7940061a5d7010104898613f71521396287
annisaeka123 <annisaekapuspitaaa@gmail.com>
2024-02-18 10:29:30 +0700

    jobsheet-1

--- jobsheet-1/pemilihan.java
+++ jobsheet-1/pemilihan.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 /**
  * pemilihan
  */
-public class pemilihann {
+public class pemilihan {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
