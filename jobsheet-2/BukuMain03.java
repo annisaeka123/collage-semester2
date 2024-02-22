fcfc59c72eee46bf7200c95bd0c41e8c9b6373a8
annisaeka123 <annisaekapuspitaaa@gmail.com>
2024-02-22 21:52:11 +0700

    bukuMain03

---
+++ jobsheet-2/BukuMain03.java
@@ -0,0 +1,19 @@
+/**
+ * BukuMain03
+ */
+public class BukuMain03 {
+
+    public static void main(String[] args) {
+        Buku03 bk1 = new Buku03();
+        bk1.judul = "Today Ends Tomorrow Comes";
+        bk1.pengarang = "Denanda Pratiwi";
+        bk1.halaman = 198;
+        bk1.stok = 13;
+        bk1.harga = 71000;
+
+        bk1.tampilInformasi();
+        bk1.terjual(5);
+        bk1.gantiHarga(60000);
+        bk1.tampilInformasi();
+    }
+}
