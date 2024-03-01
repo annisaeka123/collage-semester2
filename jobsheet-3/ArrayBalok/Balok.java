1812e2356661fc7fbe5670c2e2a10b380f73af27
annisaeka123 <annisaekapuspitaaa@gmail.com>
2024-03-01 16:10:36 +0700

    Balok

---
+++ jobsheet-3/ArrayBalok/Balok.java
@@ -0,0 +1,23 @@
+package ArrayBalok;
+
+/**
+ * Balok
+ */
+public class Balok {
+
+    public int panjang;
+    public int lebar;
+    public int tinggi;
+
+    public Balok(int p, int l, int t)
+    {
+        panjang = p;
+        lebar = l;
+        tinggi = t;
+    }
+
+    public int hitungVolume()
+    {
+        return panjang * lebar * tinggi;
+    }
+}
