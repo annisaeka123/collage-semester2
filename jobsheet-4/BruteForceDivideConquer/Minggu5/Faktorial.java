77eda40b0fee3f918aaa64d322725720ba78c238
annisaeka123 <annisaekapuspitaaa@gmail.com>
2024-03-19 22:56:23 +0700

    pangkat

--- jobsheet-4/BruteForceDivideConquer/Minggu5/Faktorial.java
+++ jobsheet-4/BruteForceDivideConquer/Minggu5/Faktorial.java
@@ -7,8 +7,10 @@ public class Faktorial {
 
     int faktorialBF(int n){
         int fakto = 1;
-        for(int i=1; i <= n; i++){
-            fakto = fakto * i;
+        int i = 1;
+        while (i <= n) {
+            fakto *= i;
+            i++;
         }
         return fakto;
     }
