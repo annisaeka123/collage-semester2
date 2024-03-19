2600cd2c6875ed76414888d898019a7927d44538
annisaeka123 <annisaekapuspitaaa@gmail.com>
2024-03-20 00:59:11 +0700

    Sum

---
+++ jobsheet-4/BruteForceDivideConquer/Minggu5/Sum.java
@@ -0,0 +1,28 @@
+public class Sum {
+    public int elemen;
+    public double keuntungan[], total;
+
+    public Sum(int elemen){
+        this.elemen = elemen;
+        this.keuntungan = new double[elemen];
+        this.total = 0;
+    }
+
+    double totalBF(double arr[]){
+        for(int i = 0; i < elemen; i++){
+            total = total + arr[i];
+        }
+        return total;
+    }
+
+    double totalDC(double arr[], int l, int r){
+        if(l==r){
+            return arr[l];
+        }else  {
+            int mid = (l + r) / 2;
+            double lsum = totalDC(arr, l, mid);
+            double rsum = totalDC(arr, mid + 1, r);
+            return lsum + rsum;
+        }
+    }
+}
