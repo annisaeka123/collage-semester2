e86b6b94848b0bea6654858dc293edc51aad117b
annisaeka123 <annisaekapuspitaaa@gmail.com>
2024-04-23 20:16:27 +0700

    Latihan 1

--- jobsheet-6/TestSearching/P7/MergeSortTest/MergeSorting3.java
+++ jobsheet-6/TestSearching/P7/MergeSortTest/MergeSorting3.java
@@ -5,12 +5,12 @@ package MergeSortTest;
  */
 public class MergeSorting3 {
 
-    public void MergeSort(int[] data){
+    public void MergeSort(String[] data){
         sort(data, 0, data.length - 1);
     }
 
-    public void merge(int data[], int left, int middle, int right) {
-        int[] temp = new int[data.length];
+    public void merge(String data[], int left, int middle, int right) {
+        String[] temp = new String[data.length];
         for (int i = left; i <= right; i++){
             temp[i] = data[i];
         }
@@ -19,7 +19,7 @@ public class MergeSorting3 {
         int c = left;
 
         while (a <= middle && b <= right) {
-            if (temp[a] <= temp[b]) {
+            if (temp[a].compareTo(temp[b]) <= 0) {
                 data[c] = temp[a];
                 a++;
             } else {
@@ -34,7 +34,7 @@ public class MergeSorting3 {
         }
     }
 
-    public void sort(int data[], int left, int right) {
+    public void sort(String data[], int left, int right) {
         if (left < right) {
             int middle = (left + right) / 2;
             sort(data,  left, middle);
@@ -44,10 +44,36 @@ public class MergeSorting3 {
     }
     
 
-    public void printArray(int arr[]) {
+    public void printArray(String arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
+    }
+
+    public int sequentialSearch(String[] data, String key) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].equals(key)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int binarySearch(String[] data, String key) {
+        int left = 0;
+        int right = data.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int compareResult = data[mid].compareTo(key);
+            if (compareResult == 0) {
+                return mid;
+            } else if (compareResult < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
     }
 }
