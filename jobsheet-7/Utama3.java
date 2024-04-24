242751a39ba92cf1f483a322e6e41b760eb92c92
annisaeka123 <annisaekapuspitaaa@gmail.com>
2024-04-24 10:28:11 +0700

    modif kapasitas

--- jobsheet-7/Utama3.java
+++ jobsheet-7/Utama3.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 public class Utama3 {
 
     public static void main(String[] args) {
-        Gudang3 gudang = new Gudang3(7);
-
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan kapasitas gudang: ");
+        int kapasitas = scanner.nextInt();
+        Gudang3 gudang = new Gudang3(kapasitas);
+
         while (true) {
             System.out.println("\nMenu:");
             System.out.println("1. Tambah barang");
             System.out.println("2. Ambil barang");
             System.out.println("3. Tampilkan tumpukan barang");
-            System.out.println("4. Keluar");
+            System.out.println("4. Lihat barang teratas");
+            System.out.println("5. Keluar");
             System.out.print("Pilih operasi: ");
             int pilihan = scanner.nextInt();
             scanner.nextLine();
@@ -23,9 +26,9 @@ public class Utama3 {
                     System.out.print("Masukkan kode barang: ");
                     int kode = scanner.nextInt();
                     scanner.nextLine();
-                    System.out.println("Masukkan nama barang: ");
+                    System.out.print("Masukkan nama barang: ");
                     String nama = scanner.nextLine();
-                    System.out.println("Masukkan nama kategori: ");
+                    System.out.print("Masukkan nama kategori: ");
                     String kategori = scanner.nextLine();
                     Barang3 barangBaru = new Barang3(kode, nama, kategori);
                     gudang.tambahBarang(barangBaru);
@@ -39,8 +42,13 @@ public class Utama3 {
                     gudang.tampilkanBarang();
                     break; 
 
-                case 4:
+                case 4: 
+                    gudang.lihatBarangTeratas();
+                    break; 
+
+                case 5:
                     break;
+                    
                 default:
                     System.out.println("Pilihan tidak valid. Silakan coba lagi");
             }
