af2a5cc70ff1bc7e25b7ea80b39cbb7e8d7d62b4
annisaeka123 <annisaekapuspitaaa@gmail.com>
2024-04-28 21:26:37 +0700

    modif queue overflow dan queue underflow

--- jobsheet-8/Praktikum 1/Queue.java
+++ jobsheet-8/Praktikum 1/Queue.java
@@ -67,6 +67,8 @@ public class Queue {
     public void Enqueue(int dt) {
         if (IsFull()) {
             System.out.println("Queue sudah penuh");
+            System.out.println("Program dihentikan karena queue overflow.");
+            System.exit(0);
         } else {
             if (IsEmpty()) {
                 front = 0;
@@ -85,6 +87,8 @@ public class Queue {
         int dt = 0;
         if(IsEmpty()) {
             System.out.println("Queue masih kososng");
+            System.out.println("Program dihentikan karena queue underflow.");
+            System.exit(0);
         } else {
             dt = data[front];
             size--;
