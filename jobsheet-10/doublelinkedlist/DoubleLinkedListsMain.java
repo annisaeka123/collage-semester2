e22dae4789953da40d2788f3afecd464bbfc5da5
annisaeka123 <annisaekapuspitaaa@gmail.com>
2024-05-22 03:34:44 +0700

    percobaan 3

--- jobsheet-10/doublelinkedlist/DoubleLinkedListsMain.java
+++ jobsheet-10/doublelinkedlist/DoubleLinkedListsMain.java
@@ -24,6 +24,7 @@ public class DoubleLinkedListsMain {
         dll.print();
         System.out.println("Size : " + dll.size());
         System.out.println("============================================");
+        System.out.println("============================================");
         dll.addLast(50);
         dll.addLast(40);
         dll.addLast(10);
@@ -42,5 +43,24 @@ public class DoubleLinkedListsMain {
         dll.remove(1);
         dll.print();
         System.out.println("Size : " + dll.size());
+        System.out.println("============================================");
+        dll.clear();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("============================================");
+        System.out.println("=============================================");
+        dll.addFirst(3);
+        dll.addLast(4);
+        dll.addFirst(7);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("=============================================");
+        dll.add(40, 1);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("=============================================");
+        System.out.println("Data awal pada Linked Lists adalah: " + dll.getFirst());
+        System.out.println("Data akhir pada Linked Lists adalah: " + dll.getLast());
+        System.out.println("Data indeks ke-1 pada Linked Lists adalah: " + dll.get(1));
     }
 }
