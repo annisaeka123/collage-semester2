fb250c604cf639cf78bed501e00ee155f9cff5b7
annisaeka123 <annisaekapuspitaaa@gmail.com>
2024-05-22 02:04:08 +0700

    percobaan 1

---
+++ jobsheet-10/doublelinkedlist/Node.java
@@ -0,0 +1,16 @@
+package doublelinkedlist;
+
+/**
+ * Node
+ */
+public class Node {
+
+    int data;
+    Node prev, next;
+
+    Node(Node prev, int data, Node next) {
+        this.prev=prev;
+        this.data=data;
+        this.next=next;
+    }
+}
