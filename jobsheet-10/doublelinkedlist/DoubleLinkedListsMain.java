7447a69a3dcfac3725c1b8c2182b59d7c4fc3a67
annisaeka123 <annisaekapuspitaaa@gmail.com>
2024-05-22 02:48:36 +0700

    percobaan 2

--- jobsheet-10/doublelinkedlist/DoubleLinkedListsMain.java
+++ jobsheet-10/doublelinkedlist/DoubleLinkedListsMain.java
@@ -23,5 +23,24 @@ public class DoubleLinkedListsMain {
         dll.clear();
         dll.print();
         System.out.println("Size : " + dll.size());
+        System.out.println("============================================");
+        dll.addLast(50);
+        dll.addLast(40);
+        dll.addLast(10);
+        dll.addLast(20);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("=============================================");
+        dll.removeFirst();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("=============================================");
+        dll.removeLast();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("=============================================");
+        dll.remove(1);
+        dll.print();
+        System.out.println("Size : " + dll.size());
     }
 }
